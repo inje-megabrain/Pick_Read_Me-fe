3cfcf141c570ccdb00db3ac3869cbd44a84efea4
narinn-star <skfls2618@naver.com>
2023-06-10 22:23:21 +0900

    Feat : [#14] ReadmeFile Viewer
    - @uiw/react-md-editor library 사용
      - MDEditor.Markdown 사용

--- src/Components/Write/ReadMe.tsx
+++ src/Components/Write/ReadMe.tsx
@@ -1,4 +1,5 @@
 import { useFetchReadme } from '../../Query/post';
+import MDEditor from '@uiw/react-md-editor';
 
 interface ReadMe {
   repo: string;
@@ -9,7 +10,13 @@ const ReadMe = ({ repo, clicked }: ReadMe) => {
   return (
     <>
       <div className="mx-auto mt-10 w-10/12 flex flex-col text-gray-800 border-gray-300 p-4 shadow-lg max-w-2xl">
-        <div>{readme}</div>
+        <div>
+          {readme ? (
+            <MDEditor.Markdown source={readme} />
+          ) : (
+            <>가져올 Repository명을 입력해주시고, 버튼을 눌러주세요!</>
+          )}
+        </div>
       </div>
     </>
   );
