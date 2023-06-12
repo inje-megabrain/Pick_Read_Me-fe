5a7a6814d28fe74400c37d5c7f6601deff7da814
narinn-star <skfls2618@naver.com>
2023-06-13 00:09:37 +0900

    Feat : [#14] Component to Image
    - dom-to-image library 사용
      - cors 오류로 출력 불가
      - server로 보내기 or html2canvas 사용 예정

--- src/Components/Write/ReadMe.tsx
+++ src/Components/Write/ReadMe.tsx
@@ -1,5 +1,7 @@
 import { useFetchReadme } from '../../Query/post';
 import MDEditor from '@uiw/react-md-editor';
+import domtoimage from 'dom-to-image';
+import { useRef } from 'react';
 
 interface ReadMe {
   repo: string;
@@ -7,12 +9,32 @@ interface ReadMe {
 }
 const ReadMe = ({ repo, clicked }: ReadMe) => {
   const { data: readme } = useFetchReadme(repo, clicked);
+  const read = useRef();
+  const capImage = () => {
+    if (document.getElementById('read')) {
+      domtoimage
+        .toBlob(document.getElementById('read') as HTMLElement)
+        .then((png) => {
+          console.log(png);
+        });
+    }
+  };
   return (
     <>
       <div className="mx-auto mt-10 w-10/12 flex flex-col text-gray-800 border-gray-300 p-4 shadow-lg max-w-2xl">
         <div>
           {readme ? (
-            <MDEditor.Markdown source={readme} />
+            <>
+              <div id="read">
+                <MDEditor.Markdown source={readme} />
+              </div>
+              <button
+                className="btn btn-outline btn-secondary"
+                onClick={capImage}
+              >
+                이얍
+              </button>
+            </>
           ) : (
             <>가져올 Repository명을 입력해주시고, 버튼을 눌러주세요!</>
           )}
