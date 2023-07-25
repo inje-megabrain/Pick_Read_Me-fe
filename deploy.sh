d60233533f6118ec03cb06b308a0a4a1ac53a7a8
narinn-star <skfls2618@naver.com>
2023-07-25 22:16:12 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,11 @@
-echo "> [$(date +%y-%m-%d/%H:%M)] Deploy React" >> /home/ubuntu/front/deloy.log
+# echo "> [$(date +%y-%m-%d/%H:%M)] Deploy React" >> /home/ubuntu/front/deloy.log
 
-pm2 kill
-pm2 serve /home/front/build 3000 --spa --name front
+# pm2 kill
+# pm2 serve /home/front/build 3000 --spa --name front
+REPOSITORY=/home/ubuntu/front
+
+cd $REPOSITORY 
+
+sudo yarn install 
+
+sudo npx pm2 reload all
